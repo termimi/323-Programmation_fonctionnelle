4219f7391ff50926e927c16f632262517eeb09cc
termimi <123395118+termimi@users.noreply.github.com>
2024-09-25 10:49:22 +0200

    feat(exo maximum): Remplacer les variables mutables par des immutables

--- perso/maximum/maximum/Program.cs
+++ perso/maximum/maximum/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 4 players
+List<Player> players = new List<Player>()
+{
+    new Player("Joe", 32),
+    new Player("Jack", 39),
+    new Player("William", 37),
+    new Player("Averell", 25)
+};
+
+// Initialize search
+Player elder = players.First();
+int biggestAge = elder.Age;
+
+
+// search
+foreach (Player p in players)
+{
+    if (p.Age > biggestAge) // memorize new elder
+    {
+        elder = p;
+        biggestAge = p.Age; // for future loops
+    }
+}
+Player p1 = players[0].Age > players[1].Age ? players[0] : players[1];
+Player p2 = players[2].Age > players[3].Age ? players[2]: players[3];
+Player p3 = p2.Age > p1.Age ? p2 : p1;
+
+Console.WriteLine($"Le plus agé est {p3.Name} qui a {p3.Age} ans");
+
+Console.ReadKey();
+
+public class Player
+{
+    private readonly string _name;
+    private readonly int _age;
+
+    public Player(string name, int age)
+    {
+        _name = name;
+        _age = age;
+    }
+
+    public string Name => _name;
+
+    public int Age => _age;
+}
