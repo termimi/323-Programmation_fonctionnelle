1b8f02c8b4f24b116423176090a527f59ca9be2a
termimi <123395118+termimi@users.noreply.github.com>
2024-10-02 10:45:38 +0200

    Exo fileCount oke

--- perso/getDirectories/getAllFiles/Program.cs
+++ perso/getDirectories/getAllFiles/Program.cs
@@ -1,1 +1,27 @@
-﻿if (File.Exists()) ;
+﻿string directoryPath = @"C:\Temp";
+
+(int filesNumber,int directoriesNumber)GetFilesAndDirectoryNumber(string path)
+{
+    int _totalFile = 0;
+    int _totalFolder = 0;
+    FileInfo[] files = null;
+    DirectoryInfo d = new DirectoryInfo($@"{path}");
+    List<DirectoryInfo> subDirectories = d.EnumerateDirectories().ToList();
+
+    _totalFolder += subDirectories.Count;
+    files = d.GetFiles();
+    _totalFile = files.Count();
+
+    if (subDirectories.Count() > 0)
+    {
+        subDirectories.ForEach(d =>
+        {
+            (int totalFileInSubDir,int totaFolderInSubDir) = GetFilesAndDirectoryNumber(d.FullName);
+            _totalFile += totalFileInSubDir;
+            _totalFolder += totaFolderInSubDir;
+        });
+    }
+    return (_totalFile, _totalFolder);
+}
+(int totalFiles, int totalDirectories) = GetFilesAndDirectoryNumber(directoryPath);
+Console.WriteLine($"Nombre de dossier : {totalDirectories},  nombre de fichier {totalFiles}");
